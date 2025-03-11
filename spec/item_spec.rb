173f5ea4c19a1f3e9811d980f21dda39017d1f17
Stephanie Guerrero <sguerrero0991@gmail.com>
2025-03-11 11:54:36 -0500

    Adds unpopular_items test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -68,4 +68,29 @@ RSpec.describe Item do
         expect(item1.current_high_bid).to eq(22)
        
     end
+
+    it 'filters for unpopular items' do
+        auction = Auction.new
+        item1 = Item.new('Chalkware Piggy Bank')
+        item2 = Item.new('Bamboo Picture Frame')
+        item3 = Item.new('Homemade Chocolate Chip Cookies')
+        item4 = Item.new('2 Days Dogsitting')
+        item5 = Item.new('Forever Stamps')
+
+        attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+        
+        auction.add_item(item1)
+        auction.add_item(item2)
+        auction.add_item(item3)
+        auction.add_item(item4)
+        auction.add_item(item5)
+ 
+        item1.add_bid(attendee2, 20)
+        item1.add_bid(attendee1, 22)            #`unpopular_items` are those items which have no bids.
+        item3.add_bid(attendee2, 15)
+
+        expect(auction.unpopular_items).to eq([item2, item4, item5])
+    end
 end
