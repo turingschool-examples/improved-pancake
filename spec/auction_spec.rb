ce34e9e91f1ddf79d530b81ac513ac944ce878e2
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-11 11:23:16 -0500

    tedted the potential_revenue method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -41,16 +41,32 @@ RSpec.describe Auction do
       @auction.add_item(@item4)
       @auction.add_item(@item5)
 
-     expect(@auction.unpopular_items).to eq([@item1, @item2, @item3, @item4, @item5]) 
+      expect(@auction.unpopular_items).to eq([@item1, @item2, @item3, @item4, @item5]) 
 
-     @item4.add_bid(@attendee3, 50)
-     @item1.add_bid(@attendee2, 20)
+      @item4.add_bid(@attendee3, 50)
+      @item1.add_bid(@attendee2, 20)
 
-     expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+      expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
 
-     @item3.add_bid(@attendee2, 15)
+      @item3.add_bid(@attendee2, 15)
 
-     expect(@auction.unpopular_items).to eq([@item2, @item5])
-   
+      expect(@auction.unpopular_items).to eq([@item2, @item5])
     end
+
+    it 'calculated the potential_revenue' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item3.add_bid(@attendee2, 15)
+      @item4.add_bid(@attendee3, 50)
+
+      expect(@auction.potential_revenue).to eq(87)
+    end
+
+   
 end
